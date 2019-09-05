45e9c4bfdb430071ba75f1678b350c327d274690
R. S. Doiel <rsdoiel@gmail.com>
2019-09-05 10:53:39 -0700

    Quick Save

--- scripts/CL.js
+++ scripts/CL.js
@@ -206,7 +206,8 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status == 200) {
                     let data = xhr.responseText;
-                    if (contentType === "application/json") {
+                    console.log(`DEBUG xhr.responseText '${xhr.responseText}'`);
+                    if (contentType === "application/json" && data !== "") {
                         data = JSON.parse(xhr.responseText);
                     }
                     callbackFn(data, "");
