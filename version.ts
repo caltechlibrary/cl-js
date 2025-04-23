02a5a18cf3aba9aca79dfd864383e5a7632d695e
R. S. Doiel <rsdoiel@gmail>
2025-04-23 09:45:58 -0700

    fix copyright year in license

--- version.ts
+++ version.ts
@@ -2,7 +2,7 @@
 
 export const version = '1.0.0',
 releaseDate = '2025-04-23',
-releaseHash = '41f595d',
+releaseHash = '3ca4e66',
 licenseText = `
 Copyright (c) 2025, Caltech
 All rights not granted herein are expressly reserved by Caltech.
