1b00c7c4ea8e64c6cb64567aef3d8e73fa764494
R. S. Doiel <rsdoiel@gmail>
2025-04-23 09:31:46 -0700

    commiting to main

--- version.ts
+++ version.ts
@@ -2,7 +2,7 @@
 
 export const version = '1.0.0',
 releaseDate = '2025-04-23',
-releaseHash = 'bf5c0ab',
+releaseHash = '75a0f52',
 licenseText = `
 Copyright (c) 2019, Caltech
 All rights not granted herein are expressly reserved by Caltech.
